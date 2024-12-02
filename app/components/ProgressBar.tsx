aef518e1a5ce1b73af2ad0261facbe882c7ea261
SimNed <simon.nedjari@outlook.com>
2024-12-02 17:45:21 +0100

    add component, add progress freom shadcn

---
+++ app/components/ProgressBar.tsx
@@ -0,0 +1,47 @@
+"use client";
+
+import { Progress } from "@/components/ui/progress";
+import { useEffect, useRef, useState } from "react";
+
+interface ProgressBarInterface {
+  duration: number;
+  onComplete: () => void;
+}
+
+const ProgressBar = ({ duration, onComplete }: ProgressBarInterface) => {
+  const [progress, setProgress] = useState(0);
+  const intervalRef = useRef<NodeJS.Timeout>();
+  const startTimeRef = useRef<number>();
+
+  useEffect(() => {
+    setProgress(0);
+    startTimeRef.current = Date.now();
+
+    const tick = () => {
+      const elapsedTime = Date.now() - startTimeRef.current!;
+      const newProgress = Math.min(
+        (elapsedTime / (duration * 1000)) * 100,
+        100
+      );
+
+      setProgress(newProgress);
+
+      if (newProgress >= 100) {
+        clearInterval(intervalRef.current);
+        onComplete();
+      }
+    };
+
+    intervalRef.current = setInterval(tick, 50);
+
+    return () => {
+      if (intervalRef.current) {
+        clearInterval(intervalRef.current);
+      }
+    };
+  }, [duration, onComplete]);
+
+  return <Progress value={progress} />;
+};
+
+export default ProgressBar;
